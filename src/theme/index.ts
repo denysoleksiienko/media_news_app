f711c70d8a2175a5b4fc5053640760c6b411e4e8
Denys <den.effective@gmail.com>
2024-05-15 17:48:50 +0300

    remove unused firestore, setup theme (fonts, colors, sizes)

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  fontFamily: {
+    extraLight: 'Roboto-Thin',
+    light: 'Roboto-Light',
+    regular: 'Roboto-Regular',
+    medium: 'Rotobo-Medium',
+    bold: 'Roboto-Bold',
+  },
+  colors: {
+    lightGray: '#A4A9AE26',
+    gray: '#8E949A',
+    red: '#FF6363',
+    blue: '#FF6363',
+    lightBlue: '#456EFE4D',
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 16,
+    md: 17,
+    lg: 20,
+    xl: 24,
+  },
+};
